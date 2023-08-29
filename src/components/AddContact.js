67c6002c78845a9fee2650b2ceaa0773897d4ac7
oakekywe <oakekywe.zotefamily@gmail.com>
2023-08-29 16:37:16 +0630

    [new] add contact feature

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,21 +1,66 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const AddContact = () => {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const contact = useSelector((state) => state);
+  const checkEmail = contact.find((c) => c.email === email && email);
+  const checkPhone = contact.find(
+    (c) => c.number === parseInt(number) && number
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name || !email || !number) {
+      toast.warning("Please fill all the fields");
+    }
+    if (checkEmail) {
+      toast.warning("Email already Exist");
+    }
+    if (checkPhone) {
+      toast.warning("Phone already Exist");
+    }
+    let data = {
+      id: contact[contact.length - 1].id + 1,
+      name,
+      email,
+      number,
+    };
+    dispatch({ type: "ADD_CONTACT", payload: data });
+    toast.success("Successfully Added!");
+    navigate("/");
+  };
+
   return (
     <div className="container">
       <h1 className="display-3 my-5 text-center text-uppercase">Add Contact</h1>
       <div className="row">
         <div className="col-md-6 shadow mx-auto p-5">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group mb-3">
-              <input type="text" className="form-control" placeholder="Name" />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
             <div className="form-group my-3">
               <input
                 type="email"
                 className="form-control"
                 placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="form-group my-3">
@@ -23,6 +68,8 @@ const AddContact = () => {
                 type="number"
                 className="form-control"
                 placeholder="Phone Number"
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
               />
             </div>
             <div className="form-group mt-4">
--- src/components/redux/reducers/contactReducer.js
+++ src/components/redux/reducers/contactReducer.js
@@ -1,17 +1,23 @@
+
+
 const initialState = [
   {
     id: 0,
     name: "Mg mg",
     number: 415151,
+    email: "mm@gmail.com",
   },
   {
     id: 1,
     name: "K K",
     number: 1414141,
+    email: "kk@gmail.com",
   },
 ];
 const contactReducer = (state = initialState, action) => {
   switch (action.type) {
+    case "ADD_CONTACT":
+      return [...state, action.payload];
     default:
       return state;
   }
