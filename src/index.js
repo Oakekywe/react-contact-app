3dd29a96b06ec353654564f58a001fc6e9944413
oakekywe <oakekywe.zotefamily@gmail.com>
2023-08-29 16:01:45 +0630

    [add] redux setup

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from "redux";
+import contactReducer from "./components/redux/reducers/contactReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from "react-redux";
 
+const store = createStore(contactReducer, composeWithDevTools());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  </Provider>
 );
